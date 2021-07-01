ef2e016af0089b0b6b18200d76544e52dff83946
Kartik <kartikprajapati987@gmail.com>
2021-07-02 02:54:16 +0530

    Add controls

--- dist/content.js
+++ dist/content.js
@@ -1,19 +1,22 @@
+document.getElementById('_webengage_script_tag')?.remove();
+
 function cleanPage() {
-    document.querySelector("#ember242 > section")?.remove();
-    document.querySelector("#ember242 > header")?.remove();
-    document.querySelector("#content-regions > div")?.remove();
-    document.querySelector("#content-regions > header > div > div.large-12.columns > div")?.remove();    
-    document.querySelector("#cc-footer-div")?.remove();
-    document.querySelector("#problem-comments > div > div")?.remove();
-    document.querySelector("#content-regions > aside")?.remove();
-    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > span")?.remove();
-    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > i")?.remove();
+    document.querySelector("#ember242 > section").style.display = "none";
+    document.querySelector("#ember242 > header").style.display = "none";
+    document.querySelector("#content-regions > div").style.display = "none";
+    document.querySelector("#content-regions > header > div > div.large-12.columns > div").style.display = "none";
+    document.querySelector("#content-regions > header > div > div.large-12.columns > div").className = "";
+    document.querySelector("#cc-footer-div").style.display = "none";
+    document.querySelector("#problem-comments > div > div").style.display = "none";
+    document.querySelector("#content-regions > aside").style.display = "none";
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > span").style.display = "none";
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > i").style.display = "none";
     if (document.querySelector("#problem-statement")?.firstElementChild.nodeName === 'H3') {
-        document.querySelector("#problem-statement")?.firstElementChild?.remove();        
+        document.querySelector("#problem-statement").firstElementChild.style.display = "none";
     }
-    document.querySelector("#problem-statement > aside.problem-info.mathjax-support")?.remove();
-    document.querySelector("#problem-statement > aside")?.remove();
-    document.querySelector("#problem-statement > div")?.remove();    
+    document.querySelector("#problem-statement > aside.problem-info.mathjax-support").style.display = "none";
+    document.querySelector("#problem-statement > aside").style.display = "none";
+    document.querySelector("#problem-statement > div").style.display = "none";
     document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.boxShadow = 'none';
     document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.width = '100%';
     document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.paddingRight = '0';
@@ -27,6 +30,141 @@ function cleanPage() {
     document.querySelector("body header > div").style.maxWidth = "100%";
 }
 
+function savePage() {
+    document.querySelector("#ember242 > section").style.display2 = document.querySelector("#ember242 > section").style.display;
+    document.querySelector("#ember242 > header").style.display2 = document.querySelector("#ember242 > header").style.display;
+    document.querySelector("#content-regions > div").style.display2 = document.querySelector("#content-regions > div").style.display;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > div").style.display2 = document.querySelector("#content-regions > header > div > div.large-12.columns > div").style.display;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > div").className2 = document.querySelector("#content-regions > header > div > div.large-12.columns > div").className;
+    document.querySelector("#cc-footer-div").style.display2 = document.querySelector("#cc-footer-div").style.display;
+    document.querySelector("#problem-comments > div > div").style.display2 = document.querySelector("#problem-comments > div > div").style.display;
+    document.querySelector("#content-regions > aside").style.display2 = document.querySelector("#content-regions > aside").style.display;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > span").style.display2 = document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > span").style.display;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > i").style.display2 = document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > i").style.display;
+    if (document.querySelector("#problem-statement")?.firstElementChild.nodeName === 'H3') {
+        document.querySelector("#problem-statement").firstElementChild.style.display2 = document.querySelector("#problem-statement").firstElementChild.style.display;
+    }
+    document.querySelector("#problem-statement > aside.problem-info.mathjax-support").style.display2 = document.querySelector("#problem-statement > aside.problem-info.mathjax-support").style.display;
+    document.querySelector("#problem-statement > aside").style.display2 = document.querySelector("#problem-statement > aside").style.display;
+    document.querySelector("#problem-statement > div").style.display2 = document.querySelector("#problem-statement > div").style.display;
+
+    document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.width2 = document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.width;
+    document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.boxShadow2 = document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.boxShadow;
+    document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.paddingRight2 = document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.paddingRight;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1").style.borderBottom2 = document.querySelector("#content-regions > header > div > div.large-12.columns > h1").style.borderBottom;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > a").innerText2 = document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > a").innerText;
+    document.querySelector("#content-regions").style.margin2 = document.querySelector("#content-regions").style.margin;
+    document.querySelector("#content-regions").style.padding2 = document.querySelector("#content-regions").style.padding;
+    document.querySelector("#content-regions").style.boxShadow2 = document.querySelector("#content-regions").style.boxShadow;
+    document.querySelector("body header > div").style.margin2 = document.querySelector("body header > div").style.margin;
+    document.querySelector("body header > div").style.maxWidth2 = document.querySelector("body header > div").style.maxWidth;
+}
+
+function restorePage() {
+    document.querySelector("#ember242 > section").style.display = document.querySelector("#ember242 > section").style.display2;
+    document.querySelector("#ember242 > header").style.display = document.querySelector("#ember242 > header").style.display2;
+    document.querySelector("#content-regions > div").style.display = document.querySelector("#content-regions > div").style.display2;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > div").style.display = document.querySelector("#content-regions > header > div > div.large-12.columns > div").style.display2;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > div").className = document.querySelector("#content-regions > header > div > div.large-12.columns > div").className2;
+    document.querySelector("#cc-footer-div").style.display = document.querySelector("#cc-footer-div").style.display2;
+    document.querySelector("#problem-comments > div > div").style.display = document.querySelector("#problem-comments > div > div").style.display2;
+    document.querySelector("#content-regions > aside").style.display = document.querySelector("#content-regions > aside").style.display2;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > span").style.display = document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > span").style.display2;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > i").style.display = document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > i").style.display2;
+    if (document.querySelector("#problem-statement")?.firstElementChild.nodeName === 'H3') {
+        document.querySelector("#problem-statement").firstElementChild.style.display = document.querySelector("#problem-statement").firstElementChild.style.display2;
+    }
+    document.querySelector("#problem-statement > aside.problem-info.mathjax-support").style.display2 = document.querySelector("#problem-statement > aside.problem-info.mathjax-support").style.display2;
+    document.querySelector("#problem-statement > aside").style.display2 = document.querySelector("#problem-statement > aside").style.display2;
+    document.querySelector("#problem-statement > div").style.display2 = document.querySelector("#problem-statement > div").style.display2;
+
+    document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.width = document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.width2;
+    document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.boxShadow = document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.boxShadow2;
+    document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.paddingRight = document.querySelector("#content-regions > section.content-area.small-8.columns.pl0").style.paddingRight2;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1").style.borderBottom = document.querySelector("#content-regions > header > div > div.large-12.columns > h1").style.borderBottom2;
+    document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > a").innerText = document.querySelector("#content-regions > header > div > div.large-12.columns > h1 > a").innerText2;
+    document.querySelector("#content-regions").style.margin = document.querySelector("#content-regions").style.margin2;
+    document.querySelector("#content-regions").style.padding = document.querySelector("#content-regions").style.padding2;
+    document.querySelector("#content-regions").style.boxShadow = document.querySelector("#content-regions").style.boxShadow2;
+    document.querySelector("body header > div").style.margin = document.querySelector("body header > div").style.margin2;
+    document.querySelector("body header > div").style.maxWidth = document.querySelector("body header > div").style.maxWidth2;
+}
+
+function enableDarkMode() {
+    document.querySelector('html').style.filter = "invert(0.94) hue-rotate(180deg)";
+    document.querySelector('body').style.filter = "invert(0.10)";
+    document.querySelector("#content-regions").style.boxShadow = "none";
+}
+
+function disableDarkMode() {
+    document.querySelector('html').style.filter = "none";
+    document.querySelector('body').style.filter = "none";
+    document.querySelector("#content-regions").style.boxShadow = "0 2px 10px 2px rgb(0 0 0 / 10%)";
+}
+
+let zen = false;
+function toggleZen() {
+    if (zen) {
+        restorePage();
+    } else {
+        savePage();
+        cleanPage();
+    }
+    zen = !zen;
+}
+
+let dark = false;
+function toggleDark() {
+    if (dark) {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+    dark = !dark;
+}
+
+const defaultZen = Boolean(window.localStorage.getItem('zen')) || true;
+const defaultDark = Boolean(window.localStorage.getItem('dark')) || false;
 setTimeout(async () => {
-    cleanPage();
-}, 2000);
+    if (defaultZen) {
+        toggleZen();
+    }
+    if (defaultDark) {
+        toggleDark();
+    }
+}, 4000);
+
+window.addEventListener('keydown', (e) => {
+    if ((e.ctrlKey || e.metaKey) && e.key == 'i') {
+        toggleZen();
+    }
+});
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        switch(message.type) {
+            case "zen":
+                if (message.value === zen) break;
+                if (window.localStorage.getItem('zen')) {
+                    window.localStorage.setItem('zen', !Boolean(window.localStorage.getItem('zen')));
+                } else {
+                    window.localStorage.setItem('zen', !defaultZen);
+                }
+                toggleZen();
+                break;
+            case "dark":
+                if (message.value === dark) break;
+                if (window.localStorage.getItem('dark')) {
+                    window.localStorage.setItem('dark', !Boolean(window.localStorage.getItem('dark')));
+                } else {
+                    window.localStorage.setItem('dark', !defaultDark);
+                }
+                toggleDark();
+            break;
+        }
+    }
+);
+
+// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+//     alert(response.farewell);
+// });
--- src/App.js
+++ src/App.js
@@ -1,8 +1,147 @@
+/* global chrome */
+import { useEffect, useState } from "react";
+
+const style = {
+  space1: {
+    marginBottom: "10px"
+  },
+  radioTitle: {
+    marginBottom: "20px",
+    fontSize: "15px"
+  },
+  space3: {
+    marginBottom: "30px"
+  },
+  gridCol2: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr"
+  },
+  radioLabel: {
+    paddingLeft: "10px",
+    fontSize: "15px"
+  },
+  flexCenter: {
+    display: "flex",
+    alignItems: "center"
+  },
+  mp0: {
+    margin: "0",
+    padding: "0"
+  }
+}
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
 
 function App() {
+  const [zenMode, setZenMode] = useLocalStorage('zen', true);
+  const [darkMode, setDarkMode] = useLocalStorage('dark', false);
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { type: "zen", value: zenMode });
+    });
+  }, [zenMode]);
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { type: "dark", value: darkMode });
+    });
+  }, [darkMode]);
+
+  const handleZenModeToggle = (e) => {
+    const { value } = e.target;
+    setZenMode(value === "ON");
+  }
+
+  const handleDarkModeToggle = (e) => {
+    const { value } = e.target;
+    setDarkMode(value === "ON");
+  }
+
   return (
     <div className="App">
-      Kartik
+      <form>
+        <div style={style.space3}>
+          <div style={style.radioTitle}>{"Default Mode"}</div>
+          <div style={style.gridCol2}>
+            <div style={style.flexCenter}>
+              <input
+                style={style.mp0}
+                type="radio"
+                id="zen"
+                name="toggle_default"
+                value="ON"
+                onChange={handleZenModeToggle}
+                checked={zenMode}
+              />
+              <label for="zen" style={style.radioLabel}>Zen</label>
+            </div>
+            <div style={style.flexCenter}>
+              <input
+                style={style.mp0}
+                type="radio"
+                id="normal"
+                name="toggle_default"
+                value="OFF"
+                onChange={handleZenModeToggle}
+                checked={!zenMode}
+              />
+              <label for="normal" style={style.radioLabel}>Normal</label>
+            </div>
+          </div>
+        </div>
+        <div style={style.space3}>
+          <div style={style.radioTitle}>{"Dark Mode"}</div>
+          <div style={style.gridCol2}>
+            <div style={style.flexCenter}>
+              <input
+                style={style.mp0}
+                type="radio"
+                id="on"
+                name="toggle_dark_mode"
+                value="ON"
+                onChange={handleDarkModeToggle}
+                checked={darkMode}
+              />
+              <label for="on" style={style.radioLabel}>On</label>
+            </div>
+            <div style={style.flexCenter}>
+              <input
+                style={style.mp0}
+                type="radio"
+                id="off"
+                name="toggle_dark_mode"
+                value="OFF"
+                onChange={handleDarkModeToggle}
+                checked={!darkMode}
+              />
+              <label for="off" style={style.radioLabel}>Off</label>
+            </div>
+          </div>
+        </div>
+      </form>
+      <p style={{ color: "rgba(0, 0, 0, 0.5)" }}>Press CTRL/CMD + I to toggle between zen mode</p>
     </div>
   );
 }
